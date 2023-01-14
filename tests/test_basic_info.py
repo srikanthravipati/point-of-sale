a029af1708352e918616cac052ee315e4d0c709e
srikanthravipati <rsri131@gmail.com>
2023-01-14 13:48:06 +0000

    Refactored basic info with getters and setters

--- tests/test_basic_info.py
+++ tests/test_basic_info.py
@@ -5,14 +5,14 @@ from item_basic_info import BasicInfo
 
 class TestBasicInfo(unittest.TestCase):
     def test_constructor(self):
-        basic_info = BasicInfo("Test")
-        self.assertEqual(basic_info.get_name(), "Test")
-        self.assertEqual(basic_info.get_price(), 0.0)
+        basic_info = BasicInfo("Test", 2.0)
+        self.assertEqual(basic_info.name, "Test")
+        self.assertEqual(basic_info.price, 2.0)
 
     def test_set_price(self):
-        basic_info = BasicInfo("Test")
-        basic_info.set_price(1.0)
-        self.assertEqual(basic_info.get_price(), 1.0)
+        basic_info = BasicInfo("Test", 2.0)
+        basic_info.price = 1.0
+        self.assertEqual(basic_info.price, 1.0)
 
 
 if __name__ == "__main__":
