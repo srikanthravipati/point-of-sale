c8ad6e89e68342bdadba69e27fba181c7df0e01b
srikanthravipati <rsri131@gmail.com>
2023-01-15 19:17:21 +0000

    Inherited and made use of inherited methods

--- item_full_info.py
+++ item_full_info.py
@@ -1,26 +1,34 @@
+from numpy import uint64
 from item_basic_info import BasicInfo
 from item_offer import Offer
 
 
-class FullInfo:
+class FullInfo(BasicInfo, Offer):
     """
     Holds BasicInfo and Offer for an item-code
     basic_info: BasicInfo
     offer: Offer
     """
 
-    basic_info: BasicInfo
-    offer: Offer
-
-    def __init__(self, basic_info: BasicInfo, offer: Offer):
-        self.basic_info = basic_info
-        self.offer = offer
+    def __init__(
+        self,
+        name: str,
+        price: float = 0.0,
+        discount_running: bool = False,
+        discount_n_items: uint64 = 0,
+        discount_coeff_a: float = 0.0,
+        discount_coeff_b: float = 0.0,
+    ):
+        BasicInfo.__init__(self, name, price)
+        Offer.__init__(
+            self, discount_running, discount_n_items, discount_coeff_a, discount_coeff_b
+        )
 
     def print(self) -> None:
         print("\n")
-        self.basic_info.print()
-        self.offer.print()
+        BasicInfo.print(self)
+        Offer.print(self)
 
     def set_price_and_discount(self, price: float) -> None:
-        self.basic_info.price = price
-        self.offer.discount = self.offer.calculate_discount(price)
+        self.price = price
+        self.discount = super(FullInfo, self).calculate_discount(price)
