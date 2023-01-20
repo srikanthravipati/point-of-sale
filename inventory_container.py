d9bf5a276df149c815103b530a7e7f36bb96dac3
srikanthravipati <rsri131@gmail.com>
2023-01-20 12:36:34 +0000

    Added docstrings

--- inventory_container.py
+++ inventory_container.py
@@ -1,3 +1,4 @@
+"""Holds inventory container and related contents."""
 from typing import Any, Dict
 
 from json5 import load
@@ -9,14 +10,29 @@ INVENTORY_ITEMS: Dict[str, ItemInfo] = {}
 
 
 def set_inventory_price_and_discount_s(item_price_s: Dict[str, float]) -> None:
-    """Take price for each item-code from item_price_s \n and set price and
-    reduction for INVENTORY_ITEMS."""
+    """Set price and discount for INVENTORY_ITEMS.
+
+    Arguments:
+        item_price_s: Item prices (associated with item code)
+    """
     for item_code in item_price_s:
         price = item_price_s[item_code]
         INVENTORY_ITEMS[item_code].set_price_and_discount(price)
 
 
 def update_inventory(item_code: str, item_info: Any) -> None:
+    """Update inventory for the given item (code).
+
+    If the inventory information does not contain
+    anything about DISCOUNT for an item or number
+    of items as zero to avail discount that means
+    there is no discount running for that particular
+    item.
+
+    Arguments:
+        item_code: Item code
+        item_info: Information about the item
+    """
     name = item_info["NAME"]
     if "DISCOUNT" not in item_info or item_info["DISCOUNT"].get("N_ITEMS", 0) == 0:
         INVENTORY_ITEMS[item_code] = ItemInfo(name)
@@ -33,12 +49,18 @@ def update_inventory(item_code: str, item_info: Any) -> None:
 
 
 def read_inventory_file_and_update_inventory(inventory_file: str) -> None:
+    """Read inventory file and update inventory.
+
+    Arguments:
+        inventory_file: File containing inventory information
+    """
     with open(inventory_file) as inventory:
         inventory_dict = load(inventory)
         for item_code, item_info in inventory_dict.items():
             update_inventory(item_code, item_info)
 
 
+# Read inventory file. Raise Error if the file could not be found.
 try:
     read_inventory_file_and_update_inventory(INVENTORY_FILE)
 except FileNotFoundError:
