232c92d4a41a6d02413d3285364b880a4fdf0e60
srikanthravipati <rsri131@gmail.com>
2023-01-20 12:36:16 +0000

    Added docstrings

--- user_item.py
+++ user_item.py
@@ -1,3 +1,4 @@
+"""Holds information about user item."""
 from typing import Dict
 
 import numpy as np
@@ -6,46 +7,67 @@ from item_info import ItemInfo
 
 
 class UserItem:
-    """
-    Holds number of items and total price \n
-    for user picked items of an item-code \n
-    __count: numpy.uint64 \n
-    __total: float \n
-    """
-
-    __count: np.uint64
-    __total: float
+    """Holds no of items and total price after discount per item type."""
 
     def __init__(self):
+        """Initialize an instance."""
         self.__count = np.uint64(0)
         self.__total = 0.0
 
     def print(self) -> None:
+        """To debug."""
         print(f"Count : {self.__count}")
         print(f"Total : {self.__total}")
 
     @property
     def count(self) -> np.uint64:
+        """Get item count."""
         return self.__count
 
     def incr_count_by_one(self):
+        """Increase item count by one."""
         self.__count += np.uint64(1)
 
     @property
     def total(self) -> float:
+        """Get total price."""
         return self.__total
 
     @total.setter
     def total(self, value: float) -> None:
+        """Set total price.
+
+        Arguments:
+            value: value for total
+        """
         self.__total = value
 
     def add_item_price_to_total(self, unit_price):
+        """Update total by unit item price.
+
+        Arguments:
+            unit_price: One item price
+        """
         self.total += unit_price
 
     def apply_discount(self, discount_value: float) -> None:
+        """Subtract discount value from total.
+
+        Arguments:
+            discount_value: Discount amount
+        """
         self.total -= discount_value
 
     def scan(self, item_info: ItemInfo) -> None:
+        """To upon an item scan.
+
+        | Increase item count by one
+        | Add unit item price to total
+        | Apply (calculate and subtract) discount, if valid, from total
+
+        Arguments:
+            item_info: Information about the scanned item
+        """
         self.incr_count_by_one()
         self.add_item_price_to_total(item_info.price)
         if item_info.is_discount_valid(self.count):
@@ -53,6 +75,13 @@ class UserItem:
 
 
 def user_items_total(user_items: Dict[str, UserItem]) -> float:
+    """Compute total price.
+
+    by summing total price of different item types.
+
+    Arguments:
+        user_items: User items scanned
+    """
     total = 0.0
     for user_item in user_items.values():
         total += user_item.total
